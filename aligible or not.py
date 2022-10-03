7102b45fd36d7780bb1e5a099af9379e994a5499
aayushitiwari03 <114310486+aayushitiwari03@users.noreply.github.com>
2022-10-03 19:35:36 +0530

    Add files via upload

---
+++ aligible or not.py
@@ -0,0 +1,6 @@
+def test(age):
+    if age>=18:
+        print("The person is Eligible for Vote")
+    else:
+        print("The person is not Eligible for Vote")
+test(int(input("enter the AGE : ")))
